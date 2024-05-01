3a0cfff7771e92bc244a9551eacc6d453606f2d6
Thedarkness-bd <toisiftasrik@gmail.com>
2024-05-01 13:48:25 +0600

    Top dishes displayed from food display to food item || USED CONTEXT API ||

---
+++ frontend/src/Components/FoodDsisplay/FoodDisplay.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+import "./FoodDisplay.css";
+import { StoreContext } from "../../Context/StoreContext";
+import FoodItem from "../FoodItem/FoodItem";
+const FoodDisplay = () => {
+  const { food_list } = useContext(StoreContext);
+
+  return (
+    <div className="food-display" id="food-display">
+      <h2>Top dishes near yoou</h2>
+      <div className="food-display-items">
+        {food_list.map((item, index) => {
+          return (
+            <FoodItem
+              key={index}
+              id={item._id}
+              name={item.name}
+              description={item.description}
+              price={item.price}
+              image={item.image}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default FoodDisplay;
--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import "./Home.css";
 import Hero from "../../Components/Hero/Hero";
 import ExploreMenu from "../../Components/ExploreMenu/ExploreMenu";
+import FoodDisplay from "../../Components/FoodDsisplay/FoodDisplay";
 const Home = () => {
   const [category, setCategory] = useState("All");
   return (
     <div>
       <Hero />
       <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay />
     </div>
   );
 };
