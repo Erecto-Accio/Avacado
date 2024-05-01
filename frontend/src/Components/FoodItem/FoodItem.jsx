e416fe027fe3f8791622e4fdf16a4bae0fd99c77
Thedarkness-bd <toisiftasrik@gmail.com>
2024-05-01 14:08:14 +0600

    User can now add and remove the top dishes just added the functionality

--- frontend/src/Components/FoodItem/FoodItem.jsx
+++ frontend/src/Components/FoodItem/FoodItem.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FoodItem.css";
 import { assets } from "../../assets/assets";
 const FoodItem = ({ id, name, description, price, image }) => {
+  const [countItems, setCountItems] = useState(0);
   return (
     <div className="food-item">
       <div className="food-item-img-container">
-        <img src={image} alt="" />
+        <img className="food-item-img" src={image} alt="" />
+        {!countItems ? (
+          <img
+            src={assets.add_icon_white}
+            className="add"
+            onClick={() => setCountItems((prev) => prev + 1)}
+          />
+        ) : (
+          <div className="food-item-counter">
+            <img
+              onClick={() => setCountItems((prev) => prev - 1)}
+              src={assets.remove_icon_red}
+              alt=""
+            />
+            <p>{countItems}</p>
+            <img
+              onClick={() => setCountItems((prev) => prev + 1)}
+              src={assets.add_icon_green}
+              alt=""
+            />
+          </div>
+        )}
       </div>
       <div className="food-item-info">
         <div className="food-item-rating">
