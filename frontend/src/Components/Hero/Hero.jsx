f4e42df1a31c4121ec7d78ad8a168fdebccdd7e1
Thedarkness-bd <toisiftasrik@gmail.com>
2024-05-01 09:21:24 +0600

    Hero contents --> only text added

---
+++ frontend/src/Components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Hero.css";
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-content">
+        <h2>Order your favourite food</h2>
+        <p>
+          Choose from a delectable selection of cuisines, crafted with love and
+          passion by our talented chefs.
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
