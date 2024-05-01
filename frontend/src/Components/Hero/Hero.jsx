cb80f252b4af867527a5f2663ac1a034e35becb0
Thedarkness-bd <toisiftasrik@gmail.com>
2024-05-01 09:46:10 +0600

    Hero section done and added little animation

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -3,11 +3,14 @@ import "./Hero.css";
 const Hero = () => {
   return (
     <div className="hero">
-      <div className="hero-content">
+      <div className="hero-contents">
         <h2>Order your favourite food</h2>
         <p>
-          Choose from a delectable selection of cuisines, crafted with love and
-          passion by our talented chefs.
+          Embark on an exquisite culinary adventure with our sumptuous array of
+          dishes, meticulously curated by our team of master chefs. From
+          mouthwatering appetizers to delectable mains and irresistible
+          desserts, every bite is a celebration of flavor, freshness, and
+          culinary artistry.
         </p>
         <button>View Menu</button>
       </div>
