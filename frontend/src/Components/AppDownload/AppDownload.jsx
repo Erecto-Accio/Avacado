eebbe0124f659a7546711de0b953112ab3e7a243
Thedarkness-bd <toisiftasrik@gmail.com>
2024-05-02 22:15:22 +0600

    Added navigation also fixed some bugs

--- frontend/src/Components/AppDownload/AppDownload.jsx
+++ frontend/src/Components/AppDownload/AppDownload.jsx
@@ -3,14 +3,14 @@ import "./AppDownload.css";
 import { assets } from "../../assets/assets";
 const AppDownload = () => {
   return (
-    <div className="app-download">
+    <div className="app-download" id="app-download">
       <h3>
         For Better Experience Download Our <br /> Avacado App
       </h3>
 
       <div className="app-download-img-container">
-        <img src={assets.play_store} alt="" loading="lazy"/>
-        <img src={assets.app_store} alt="" loading="lazy"/>
+        <img src={assets.play_store} alt="" loading="lazy" />
+        <img src={assets.app_store} alt="" loading="lazy" />
       </div>
     </div>
   );
