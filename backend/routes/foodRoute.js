ed2efac501ac3341f2179b32e8bf2a12b7111127
Thedarkness-bd <toisiftasrik@gmail.com>
2024-05-09 09:33:31 +0600

    Food Route added

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -4,4 +4,20 @@ const addFoodItem = require("../controllers/food.controller");
 // Router
 const foodRouter = express.Router();
 
+// image storage engine
+const storage = multer.diskStorage({
+  destination: "uploads",
+  filename: (req, file, callback) => {
+    return callback(null, `${Date.now()}${file.originalname}`);
+  },
+});
+
+// middleware
+const upload = multer({
+  storage: storage,
+});
+
+// posting
+foodRouter.post("/add", upload.single("image"), addFoodItem);
+
 module.exports = foodRouter;
