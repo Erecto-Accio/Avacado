691d448ce9665ff0944428af13ee21ab519a8b89
Thedarkness-bd <toisiftasrik@gmail.com>
2024-05-10 12:48:27 +0600

    Admin can see all the added items in the database

--- admin/src/Pages/List/List.jsx
+++ admin/src/Pages/List/List.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
-import "./List.css"
+import React, { useEffect, useState } from "react";
+import "./List.css";
+import axios from "axios";
+import { toast } from "react-toastify";
 const List = () => {
-  return (
-    <div>List</div>
-  )
-}
+  const [list, setList] = useState([]);
 
-export default List
+  const fetchList = async () => {
+    const response = await axios.get(
+      "http://localhost:3000/api/food/listFoods"
+    );
+    console.log(list);
+    if (response.data.success) {
+      setList(response.data.data);
+    } else {
+      toast.error("Error");
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+  return (
+    <div className="list add flex-col">
+      <h1>All food list</h1>
+
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+
+        {list.map((item, index) => {
+          return (
+            <div key={index} className="list-table-format">
+              <img src={`http://localhost:3000/images/${item.image}`} alt="" />
+              <p>{item.name}</p>
+              <p>{item.category.toUpperCase()}</p>
+              <p>${item.price}</p>
+              <p>X</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
